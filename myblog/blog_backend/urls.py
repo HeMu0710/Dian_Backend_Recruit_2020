1d36db13aedbc2d50785e0d2a3f79ff95e9a466c
holdice <hemu0710@qq.com>
2020-05-10 19:57:27 +0800

    complete the blog CRUD

--- myblog/blog_backend/urls.py
+++ myblog/blog_backend/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('blog', views.blog, name='所有博客'),
-    path('blog/<int:pk>', views.blogDetail, name='博客详情'),
-    path('blog/add', views.addBlog, name='添加博客')
+    path('blog/<int:pk>', views.blogRUD, name='博客详情、修改、删除'),
+    path('blog/add', views.addBlog, name='添加博客'),
 ]
--- myblog/blog_backend/views.py
+++ myblog/blog_backend/views.py
@@ -10,7 +10,8 @@ import json, jwt
 
 @auth_required()
 def blog(request, user):
-    blog_list = Article.objects.using('db_' + user.region).all()
+    db = 'db_' + user.region
+    blog_list = Article.objects.using(db).all()
     data = serializers.serialize('json', blog_list,
                                  fields=('article_title', 'article_author', 'created_date', 'edited_date'),
                                  use_natural_foreign_keys=True)
@@ -19,17 +20,58 @@ def blog(request, user):
 
 
 @auth_required()
-def blogDetail(request, _, pk):
+def blogRUD(request, user, pk):
+    db = 'db_' + user.region
     article = Article.objects.filter(pk=pk)
-    data = serializers.serialize('json', article, use_natural_foreign_keys=True)
-    data = json.loads(data)
-    return JsonResponse(data, safe=False)
+
+    # R,展示博客内容
+    if request.method == 'GET':
+        data = serializers.serialize('json', article, use_natural_foreign_keys=True)
+        data = json.loads(data)
+        return JsonResponse(data, safe=False)
+
+    # U,更新（修改）博客
+    if request.method == 'POST':
+        # 检查当前用户是否为博客的作者
+        if article.first().article_author == user:
+            edit_article_title = request.POST.get('article_title')
+            edit_article_content = request.POST.get('article_content')
+            Article.objects.using(db).filter(id=pk).update(article_title=edit_article_title,
+                                                           article_content=edit_article_content)
+            return JsonResponse({"status_code": 200, "message": "edit article success"})
+        else:
+            return JsonResponse({"status_code": 403, "message": "No right to edit"})
+
+    # D,删除博客
+    if request.method == 'DELETE':
+        # 检查当前用户是否为博客的作者
+        if article.first().article_author == user:
+            article.first().delete()
+            return JsonResponse({"status_code": 200, "message": "delete success"})
+        else:
+            return JsonResponse({"status_code": 403, "message": "No right to delete"})
 
 
 @auth_required()
 def addBlog(request, user):
+    db = 'db_' + user.region
     add_article_title = request.POST.get('article_title')
     add_article_content = request.POST.get('article_content')
-    Article.objects.create(article_title=add_article_title, article_content=add_article_content,article_author=user)
+    Article.objects.using(db).create(article_title=add_article_title,
+                                     article_content=add_article_content, article_author=user)
     return JsonResponse({"status_code": 200, "message": "create article success"})
 
+
+# @auth_required()
+# def editBlog(request, user, pk):
+#     db = 'db_' + user.region
+#     edit_article_title = request.POST.get('article_title')
+#     edit_article_content = request.POST.get('article_content')
+#     Article.objects.using(db).filter(id=pk).update(article_title=edit_article_title,
+#                                                    article_content=edit_article_content)
+#     return JsonResponse({"status_code": 200, "message": "edit article success"})
+#
+#
+# @auth_required
+# def deleteBlog(request, user, pk):
+#     db = 'db_' + user.region
